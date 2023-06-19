4e7170bb096403ea99288c6dc6e38ecf277d06d0
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-06-19 19:17:33 -0300

    class in process

--- Mongoose/src/app.js
+++ Mongoose/src/app.js
@@ -1,16 +1,22 @@
 import express from 'express';
-import routerProducts from './routes/products.router.js';
-import routerCarts from './routes/cart.router.js';
+import userRouter from './models/user.model.js';
+import mongoose  from 'mongoose';
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-app.use('/products/', routerProducts);
-app.use('/carts/', routerCarts)
-
-
 app.listen(8080, ()=>{
     console.log('server is listening...');
 })
+
+
+mongoose.connect('mongodb+srv://garay17:hola123@cluster0.xmyrsuv.mongodb.net/?retryWrites=true&w=majority', (error)=>{
+    if(error){
+        console.log('Cannot connect to database: ' + error)
+        process.exit()
+    }
+})
+
+
+app.use('/api/users', userRouter);
